c12737fe5efb3668fa772053879019d5098ef601
Alex <digwnews@gmail.com>
2015-09-29 13:13:19 +0300

    main layout done

--- app/src/main/java/com/lookyan/alex/translater/MainActivity.java
+++ app/src/main/java/com/lookyan/alex/translater/MainActivity.java
@@ -1,12 +1,15 @@
 package com.lookyan.alex.translater;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 
@@ -18,17 +21,23 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
-        spinner.getBackground().setColorFilter(getResources().getColor(R.color.white_trian), PorterDuff.Mode.SRC_ATOP);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        Spinner langFromSpinner = (Spinner) findViewById(R.id.lang_from_spinner);
+        langFromSpinner.getBackground().setColorFilter(getResources().getColor(R.color.white_trian), PorterDuff.Mode.SRC_ATOP);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.langs_array, R.layout.spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(this);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        langFromSpinner.setAdapter(adapter);
+
+        langFromSpinner.setOnItemSelectedListener(this);
+
+
+        Spinner langToSpinner = (Spinner) findViewById(R.id.lang_to_spinner);
+        langToSpinner.getBackground().setColorFilter(getResources().getColor(R.color.white_trian), PorterDuff.Mode.SRC_ATOP);
+
+        langToSpinner.setAdapter(adapter);
 
 
         if(savedInstanceState != null) {
@@ -51,5 +60,12 @@ public class MainActivity extends Activity implements AdapterView.OnItemSelected
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void onCardClick(View v) {
+        EditText textTranslate = (EditText)findViewById(R.id.textTranslate);
+        textTranslate.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(textTranslate, InputMethodManager.SHOW_IMPLICIT);
     }
 }
